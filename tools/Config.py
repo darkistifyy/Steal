fd341c8bd18c4697087abded12480349574d6963
! oxy <jpeg@psycho.email>
2024-08-11 02:22:27 +0100

    Create Config.py

---
+++ tools/Config.py
@@ -0,0 +1,14 @@
+from discord import Color
+class Auth:
+    token = "MTI2NDIyNTcxNjc5OTc5OTQyNw.G9rvs_.I5MxbmyycnPaif69gEA5_2i1rieneq5bf8p3Ws"
+
+class Colors:
+    BASE_COLOR = Color.pink()
+    DENY_COLOR = Color.red()
+    NEUTRAL_COLOR = BASE_COLOR
+    WARN_COLOR = Color.yellow()
+
+class Emojis:
+    APPROVE = "<:Success:1182764836682203196>"
+    WARN = "⚠️"
+    DENY = "<:Error:1182764757938339901>"
