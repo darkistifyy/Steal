9d011550f5158a17d8c06f0fa8f889e510c0bd9d
! oxy <jpeg@psycho.email>
2024-08-11 02:21:00 +0100

    Create context.py

---
+++ managers/context.py
@@ -0,0 +1,59 @@
+import discord
+
+from typing import List
+
+from discord import Message, Embed
+from discord.ext.commands import Context, Group
+
+from tools.Config import Colors, Emojis
+from tools.Paginator import Paginator
+
+class StealContext(Context):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    async def approve(self, message: str, **kwargs) -> Message:
+        embed=Embed(
+            color = Colors.BASE_COLOR,
+            description = f'{Emojis.APPROVE} {self.author.mention}: {message}'
+        )
+        await self.reply(
+            embed=embed,
+            **kwargs
+        )
+
+    async def warn(self, message: str, **kwargs) -> Message:
+            embed=Embed(
+                color = Colors.WARN_COLOR,
+                description = f'{Emojis.WARN} {self.author.mention}: {message}'
+            )
+            await self.reply(
+                embed=embed,
+                **kwargs
+            )
+    
+    async def deny(self, message: str, **kwargs) -> Message:
+        return await self.reply(
+            embed=Embed(
+                color = Colors.DENY_COLOR,
+                description = f'{Emojis.DENY} {self.author.mention}: {message}'
+            ),
+            **kwargs
+        )
+
+    async def neutral(self, message: str, **kwargs) -> Message:
+        return await self.reply(
+            embed=Embed(
+                color = Colors.NEUTRAL_COLOR,
+                description = f'{message}'
+            ),
+            **kwargs
+        )
+
+    async def paginate(self, embeds: List[discord.Embed], **kwargs) -> Message:
+        return await self.reply(
+            embed = embeds[0],
+            view  = Paginator(self, embeds),
+            **kwargs
+        )
+
