b067823a81e66c7dab77ae5e1b43f813d6eccf42
! oxy <jpeg@psycho.email>
2024-08-12 02:41:51 +0100

    Updated all tools

--- tools/Config.py
+++ tools/Config.py
@@ -3,7 +3,7 @@ class Auth:
     token = "MTI2NDIyNTcxNjc5OTc5OTQyNw.G9rvs_.I5MxbmyycnPaif69gEA5_2i1rieneq5bf8p3Ws"
 
 class Colors:
-    BASE_COLOR = Color.pink()
+    BASE_COLOR = 0xFF8FAB
     DENY_COLOR = Color.red()
     NEUTRAL_COLOR = BASE_COLOR
     WARN_COLOR = Color.yellow()
---
+++ tools/bytesio.py
@@ -0,0 +1,72 @@
+
+from PIL import Image, ImageDraw, ImageFont
+from io import BytesIO
+import textwrap
+import numpy as np
+from io import BytesIO
+import scipy.cluster
+import scipy.cluster.vq
+import binascii
+
+def caption_image(image_file, caption, font="impact.ttf"):
+	"""Captions an image.
+
+	Args:
+		image_file: A BytesIO object containing the image file.
+		caption: The caption text.
+		font: The font to use for the caption.
+
+	Returns:
+		A BytesIO object containing the captioned image.
+	"""
+
+	my_bytes = BytesIO(image_file)
+	my_bytes.seek(0)
+	img = Image.open(my_bytes)
+	draw = ImageDraw.Draw(img)
+
+	font_size = int(img.width/16)
+	font = ImageFont.truetype("impact.ttf", font_size)
+
+	caption = textwrap.fill(text=caption, width=img.width/(font_size/2))
+
+	caption_w, caption_h = img.size
+
+	draw.text(((img.width-caption_w)/2, (img.height-caption_h)/8), # position
+			caption, # text
+			(255,255,255), # color
+			font=font, # font
+			stroke_width=2, # text outline width
+			stroke_fill=(0,0,0)) # text outline color
+
+	with BytesIO() as img_bytes:
+		img.save(img_bytes, format=img.format)
+		return img_bytes.getvalue()
+
+NUM_CLUSTERS = 5
+
+def dom_color(img):
+	my_bytes = BytesIO(img)
+	my_bytes.seek(0)
+	im = Image.open(my_bytes)
+	
+	im = im.convert('RGBA')
+	im = im.resize((150, 150))
+	
+	ar = np.asarray(im)
+	shape = ar.shape
+	
+	mask = ar[:, :, 3] > 0
+	ar = ar[mask]
+	
+	ar = ar[:, :3].astype(float)
+	
+	codes, dist = scipy.cluster.vq.kmeans(ar, NUM_CLUSTERS)
+
+	vecs, dist = scipy.cluster.vq.vq(ar, codes)
+	counts, bins = np.histogram(vecs, len(codes))
+
+	index_max = np.argmax(counts)
+	peak = codes[index_max]
+	colour = binascii.hexlify(bytearray(int(c) for c in peak)).decode('ascii')
+	return colour
