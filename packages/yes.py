bdb6f409fb8bcfda79b8a1ec6de77cfb463c384c
! oxy <jpeg@psycho.email>
2024-08-18 10:26:47 +0100

    Create yes.py

---
+++ packages/yes.py
@@ -0,0 +1,7 @@
+import pkg_resources; installed_packages = [f"{d.project_name}=={d.version}" for d in pkg_resources.working_set]
+
+with open("packages.txt", "w+") as fp:
+    for package in installed_packages:
+        fp.writelines(f"{package}\n")
+
+print(installed_packages)
