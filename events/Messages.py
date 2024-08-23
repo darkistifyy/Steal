e1a2d1b7d604b6e9675a2d31f7b7f071696a0c33
! oxy <jpeg@psycho.email>
2024-08-23 09:47:58 +0100

    Add files via upload

--- events/Messages.py
+++ events/Messages.py
@@ -14,7 +14,8 @@ url_prefixes = "https://discord.com/invite/", "discord.gg/", ".gg/"
 class Messages(commands.Cog):
 	def __init__(self, bot: Steal):
 		self.bot = bot
-		self.status = False
+		self.afkstatus = False
+		self.invitestatus = False
 		
 	@Cog.listener("on_message")
 	async def filter_message_invite_event(self, message: discord.Message) -> None:
@@ -60,10 +61,13 @@ class Messages(commands.Cog):
 												color=Colors.WARN_COLOR
 											)
 										)
-										await message.delete()
+										try:
+											return await message.delete()
+										except:
+											return
 	
 	@Cog.listener("on_message")
-	async def filter_message_blacklist_event(self, message: discord.Message) -> None:
+	async def filter_message_blacklist_event(self, message: discord.Message):
 		if message.author.bot: return
 		if message.is_system(): return
 		if not message.author: return
@@ -79,29 +83,20 @@ class Messages(commands.Cog):
 
 				cur = await cursor.execute(
 					"""
-					SELECT toggle FROM wordsautomod WHERE guildid = $1 
+					SELECT * FROM wordsautomod WHERE guildid = $1 
 					""", message.guild.id
 				)
 
+				row = await cur.fetchone()
 
-				##### I WAS HERE DOING CLOSE CURSORS
-
-				toggle = await cur.fetchone()
-
-				if toggle:
-					if toggle[0] == 1:
+				if row:
+					if row[1] == 1:
 
 						if message.content.startswith(f"{self.bot.command_prefix[0]}filter words remove") or message.content.startswith(f"{self.bot.command_prefix[0]}filter words add"):
 							if message.author.guild_permissions.manage_messages:
 								return
 
-						cur = await cursor.execute(
-							"""
-							SELECT words FROM wordsautomod WHERE guildid = $1
-							""", message.guild.id
-						)
-
-						words = await cur.fetchone()
+						words = row[2]
 						await cur.close()
 
 						things = [word for word in words[0].split(",") if word]
@@ -124,7 +119,7 @@ class Messages(commands.Cog):
 		if not message.author: return
 		if isinstance(message.channel, discord.DMChannel): return
 		if message.content.startswith(f"{self.bot.command_prefix[0]}afk"): return
-		if self.status: return
+		if self.afkstatus == message.author: return
 		async with asqlite.connect("main.db") as conn:
 			async with conn.cursor() as cursor:
 				await cursor.execute(
@@ -139,7 +134,7 @@ class Messages(commands.Cog):
 				row = await cur.fetchone()
 
 				if row:
-					self.status = True
+					self.afkstatus = True
 					await cursor.execute(
 						"DELETE FROM afk WHERE guildid = $1 AND userid = $2",
 						message.guild.id, row[1],
@@ -151,7 +146,7 @@ class Messages(commands.Cog):
 							color=Colors.BASE_COLOR
 						)
 					)
-				self.status = False
+				self.afkstatus = False
 
 	@Cog.listener("on_message")
 	async def afk_user_mention_event(self, message: discord.Message) -> None:
@@ -159,7 +154,6 @@ class Messages(commands.Cog):
 		if message.is_system(): return
 		if not message.author: return
 		if isinstance(message.channel, discord.DMChannel): return
-		if self.status: return
 		async with asqlite.connect("main.db") as conn:
 			async with conn.cursor() as cursor:
 				await cursor.execute(
@@ -167,33 +161,94 @@ class Messages(commands.Cog):
 				)
 
 				cur = await cursor.execute(
-					"SELECT userid FROM afk WHERE guildid = $1", message.guild.id,
+					"SELECT * FROM afk WHERE guildid = $1 AND userid = $2",
+					message.guild.id, message.author.id,
 				)
 
 				row = await cur.fetchall()
 
 				if row:
-					if row[0]:
-						for userid in row[0]:
-							if message.author.id == userid: return
-							self.status = True
-							user = self.bot.get_user(userid)
-							if user.mentioned_in(message):
-								cur1 = await cursor.execute(
-									"SELECT status FROM afk WHERE guildid = $1 AND userid = $2", message.guild.id, userid,
+					row = row[0]
+					if row[1]:
+						user = self.bot.get_user(row[1])
+						if user.mentioned_in(message):
+							status = row[2]
+							await message.reply(
+								embed=discord.Embed(
+									description=f"{user.mention} is AFK with the status - **{status if status else "Unretriveable status"}**",
+									color=Colors.BASE_COLOR
 								)
-
-								status = await cur1.fetchone()
-
-
-								await message.reply(
-									embed=discord.Embed(
-										description=f"{user.mention} is AFK with the status - **{status[0] if status else "Unretriveable status"}**",
-										color=Colors.BASE_COLOR
-									)
-								)
-				self.status = False
+							)
 							
+	@Cog.listener("on_message_delete")
+	async def delete_snipe(self, message: discord.Message):
+		if message.author.bot:
+			return
+
+		get_snipes = self.bot.cache.get("snipe")
+		payload = [
+			{
+				"channel": message.channel.id,
+				"name": str(message.author),
+				"avatar": message.author.display_avatar.url,
+				"message": message.content,
+				"attachments": message.attachments,
+				"stickers": message.stickers,
+				"created_at": message.created_at.timestamp(),
+			}
+		]
+
+		if get_snipes:
+			temp = self.bot.cache.get("snipe")
+			temp.append(
+				{
+					"channel": message.channel.id,
+					"name": str(message.author),
+					"avatar": message.author.display_avatar.url,
+					"message": message.content,
+					"attachments": message.attachments,
+					"stickers": message.stickers,
+					"created_at": message.created_at.timestamp(),
+				}
+			)
+			return await self.bot.cache.set("snipe", temp)
+		else:
+			await self.bot.cache.set("snipe", payload)
+
+
+	@Cog.listener("on_message_edit")
+	async def edit_snipe(self, before: discord.Message, after: discord.Message):
+		if before.author.bot:
+			return
+		if before.content == after.content:
+			return
+
+		get_snipes = self.bot.cache.get("edit_snipe")
+		if get_snipes:
+			temp = self.bot.cache.get("edit_snipe")
+			temp.append(
+				{
+					"channel": before.channel.id,
+					"name": str(before.author),
+					"avatar": before.author.display_avatar.url,
+					"before": before.content,
+					"after": after.content,
+					"edited_at": after.created_at.timestamp(),
+				}
+			)
+			return await self.bot.cache.set("edit_snipe", temp)
+		else:
+			payload = [
+				{
+					"channel": before.channel.id,
+					"name": str(before.author),
+					"avatar": before.author.display_avatar.url,
+					"before": before.content,
+					"after": after.content,
+					"edited_at": after.created_at.timestamp()
+				}
+			]
+			return await self.bot.cache.set("edit_snipe", payload)
 
 async def setup(bot):
 	await bot.add_cog(Messages(bot))
