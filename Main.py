a2e1473341c9868196266f1cb27efb74fc233164
! darkist <jpeg@psycho.email>
2026-03-02 22:42:16 +0000

    async + db fixes

--- Main.py
+++ Main.py
@@ -1,29 +1,31 @@
 from __future__ import annotations
 
-from tools.Config import *
-import discord
-from discord.ext import *
-from discord.ext.commands import *
 import asqlite
+import discord
 
+from tools.Config import Auth
 from tools.Steal import Steal
 
 bot = Steal()
 
+
 @bot.event
 async def on_message(message: discord.Message):
-	async with asqlite.connect("main.db") as db:
-		async with db.cursor() as cursor:
-			await cursor.execute(
-				"CREATE TABLE IF NOT EXISTS userblacklist(userid INTEGER)"
-			)
-			cur = await cursor.execute(
-				"SELECT * FROM userblacklist WHERE userid = $1",
-				message.author.id,
-			)
-			row = await cur.fetchone()
-			if not row:
-				return await bot.process_commands(message)
+    async with asqlite.connect("main.db") as db:
+        async with db.cursor() as cursor:
+            await cursor.execute(
+                "CREATE TABLE IF NOT EXISTS userblacklist(userid INTEGER)"
+            )
+            cur = await cursor.execute(
+                "SELECT * FROM userblacklist WHERE userid = $1",
+                message.author.id,
+            )
+            row = await cur.fetchone()
+            await cursor.close()
+
+            if not row:
+                return await bot.process_commands(message)
+
 
 if __name__ == "__main__":
-	bot.run(Auth.token)
+    bot.run(Auth.token)
