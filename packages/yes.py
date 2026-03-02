a8f8402c5b68b6c3d160292d2441f0f0a0861b97
! darkist <jpeg@psycho.email>
2026-03-02 22:39:28 +0000

    package updates + dependency fixes

--- packages/yes.py
+++ packages/yes.py
@@ -1,4 +1,8 @@
-import pkg_resources; installed_packages = [f"{d.project_name}=={d.version}" for d in pkg_resources.working_set]
+import pkg_resources
+
+installed_packages = [
+    f"{d.project_name}=={d.version}" for d in pkg_resources.working_set
+]
 
 with open("packages.txt", "w+") as fp:
     for package in installed_packages:
